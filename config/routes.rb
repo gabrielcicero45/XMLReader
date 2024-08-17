d3f166eb52e20013746a3c4b47c48d29b279d942
gabrielcicero45 <gabrielcicero45@gmail.com>
2024-08-16 23:51:32 -0300

    feat: add devise views for user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'documents/new'
+  get 'documents/create'
+  get 'documents/index'
   get 'reports/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
